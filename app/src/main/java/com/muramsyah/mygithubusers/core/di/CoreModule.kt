94d6e44b2609f7632ba117deceb62e8b7ca62e12
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2021-07-02 22:04:58 +0700

    Added dependency injection in core.di and inheritance UserDatabase.kt to RoomDatabase()

---
+++ app/src/main/java/com/muramsyah/mygithubusers/core/di/CoreModule.kt
@@ -0,0 +1,52 @@
+package com.muramsyah.mygithubusers.core.di
+
+import androidx.room.Room
+import com.muramsyah.mygithubusers.core.data.source.UserRepository
+import com.muramsyah.mygithubusers.core.data.source.local.LocalDataSource
+import com.muramsyah.mygithubusers.core.data.source.local.room.UserDatabase
+import com.muramsyah.mygithubusers.core.data.source.remote.RemoteDataSource
+import com.muramsyah.mygithubusers.core.data.source.remote.network.ApiService
+import com.muramsyah.mygithubusers.core.domain.repository.IUserRepository
+import com.muramsyah.mygithubusers.core.utils.AppExecutors
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<UserDatabase>().userDao() }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            UserDatabase::class.java, "GithubUser.db"
+        ).fallbackToDestructiveMigration().build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<IUserRepository> { UserRepository(get(), get(), get()) }
+}
