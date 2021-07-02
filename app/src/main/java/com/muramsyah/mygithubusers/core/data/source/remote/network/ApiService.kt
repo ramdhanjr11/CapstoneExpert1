392aba94f1f898c1fed7f498c6fa0ab71c877f3c
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2021-07-02 20:08:50 +0700

    Added network service and response

---
+++ app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/network/ApiService.kt
@@ -0,0 +1,10 @@
+package com.muramsyah.mygithubusers.core.data.source.remote.network
+
+import com.muramsyah.mygithubusers.core.data.source.remote.response.ListUserResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("users")
+    suspend fun getListUsers(@Query("since") since: String): ListUserResponse
+}
---
+++ app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/response/ListUserResponse.kt
@@ -0,0 +1,23 @@
+package com.muramsyah.mygithubusers.core.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class ListUserResponse(
+    @field:SerializedName("login")
+    val login: String,
+
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("avatar_url")
+    val avatarUrl: String,
+
+    @field:SerializedName("url")
+    val url: String,
+
+    @field:SerializedName("followers_url")
+    val followersUrl: String,
+
+    @field:SerializedName("following_url")
+    val followingUrl: String,
+)
