9772f5719645faaa0f7da4d248bfc8d64f5265ba
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2021-07-02 22:35:00 +0700

    Added authorization token github

--- app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/RemoteDataSource.kt
@@ -17,12 +17,15 @@ class RemoteDataSource(private val apiService: ApiService) {
         val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
         val randomNumber = numbers.random()
 
+        // Github Token
+        val token = "cd64c17d1ed4aa9d1a2bd48793e9a9ca0e025a8d"
+
         Log.d("randomNumber", randomNumber.toString())
 
         // get data from api
         return flow {
             try {
-                val response = apiService.getListUsers(randomNumber.toString())
+                val response = apiService.getListUsers(randomNumber.toString(), token)
                 if (response.isNotEmpty()) {
                     emit(ApiResponse.Success(response))
                 } else {
--- app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/network/ApiService.kt
+++ app/src/main/java/com/muramsyah/mygithubusers/core/data/source/remote/network/ApiService.kt
@@ -2,9 +2,13 @@ package com.muramsyah.mygithubusers.core.data.source.remote.network
 
 import com.muramsyah.mygithubusers.core.data.source.remote.response.ListUserResponse
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("users")
-    suspend fun getListUsers(@Query("since") since: String): List<ListUserResponse>
+    suspend fun getListUsers(
+        @Query("since") since: String,
+        @Header("Authorization") token: String
+    ): List<ListUserResponse>
 }
