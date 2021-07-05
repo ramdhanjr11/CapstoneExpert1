65fdfbf4d2a1fc1a8a56a9ebc995a3ea3c1a3466
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2021-07-05 09:39:10 +0700

    Added favorite menu action in DetailActivity.kt

--- app/src/main/java/com/muramsyah/mygithubusers/detail/DetailActivity.kt
+++ app/src/main/java/com/muramsyah/mygithubusers/detail/DetailActivity.kt
@@ -1,8 +1,12 @@
 package com.muramsyah.mygithubusers.detail
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
@@ -19,9 +23,11 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailBinding
 
     private val viewModel: DetailViewModel by viewModel()
+    private var menu: Menu? = null
 
     companion object {
         const val EXTRA_DATA = "extra_data"
+        private var isFavorite = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -116,6 +122,36 @@ class DetailActivity : AppCompatActivity() {
                 textView8.visibility = View.GONE
             }
             binding.progressBar.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail_activity, menu)
+        this.menu = menu
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.favorite) {
+            if (!isFavorite) {
+                isFavorite = !isFavorite
+                setFavoriteState(true)
+                Toast.makeText(this, "Kamu menyimpan user", Toast.LENGTH_SHORT).show()
+            } else {
+                isFavorite = !isFavorite
+                setFavoriteState(false)
+                Toast.makeText(this, "Kamu menghapus user", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun setFavoriteState(state: Boolean) {
+        if (state) {
+            menu?.findItem(R.id.favorite)!!.icon = getDrawable(R.drawable.ic_baseline_favorite)
+        } else {
+            menu?.findItem(R.id.favorite)!!.icon = getDrawable(R.drawable.ic_baseline_favorite_border)
         }
     }
 }
