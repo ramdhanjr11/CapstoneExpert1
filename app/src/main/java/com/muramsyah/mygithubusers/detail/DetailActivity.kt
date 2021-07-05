3d14185fa062489e670238945c57e2d353c608fb
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2021-07-05 09:14:25 +0700

    Tidy up layout for DetailActivity.kt

--- app/src/main/java/com/muramsyah/mygithubusers/detail/DetailActivity.kt
+++ app/src/main/java/com/muramsyah/mygithubusers/detail/DetailActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
+import com.muramsyah.mygithubusers.R
 import com.muramsyah.mygithubusers.core.data.source.Resource
 import com.muramsyah.mygithubusers.core.domain.model.DetailUser
 import com.muramsyah.mygithubusers.core.domain.model.User
@@ -39,15 +40,15 @@ class DetailActivity : AppCompatActivity() {
                 when (user) {
                     is Resource.Loading -> {
                         Log.d("DetailActivity", "Loading")
-                        binding.progressBar.visibility = View.VISIBLE
+                        setVisibilityLayout(false)
                     }
                     is Resource.Success -> {
                         Log.d("DetailActivity", user.data.toString())
-                        binding.progressBar.visibility = View.GONE
+                        setVisibilityLayout(true)
                         user.data?.let { showDetailUser(it) }
                     }
                     is Resource.Error -> {
-                        binding.progressBar.visibility = View.GONE
+                        setVisibilityLayout(false)
                         Log.d("DetailActivity", "Error")
                     }
                 }
@@ -56,16 +57,65 @@ class DetailActivity : AppCompatActivity() {
     }
 
     private fun showDetailUser(detailUser: DetailUser) {
-        with(binding) {
+        with(binding.layoutItemDetailHeader) {
             Glide.with(this@DetailActivity)
                 .load(detailUser.avatarUrl)
-                .into(binding.imgUser)
+                .into(imgUser)
 
-            tvUsername.text = "@${detailUser.login}"
+            tvUsername.text = getString(R.string.username, detailUser.login)
             tvFullname.text = detailUser.name
-            tvFollowers.text = detailUser.followers.toString()
-            tvFollowing.text = detailUser.following.toString()
-            tvRepository.text = detailUser.publicRepos.toString()
+            tvFollowers.text = getString(R.string.followers, detailUser.followers)
+            tvFollowing.text = getString(R.string.followers, detailUser.following)
+            tvRepository.text = getString(R.string.followers, detailUser.publicRepos)
+            tvDetailOf.text = getString(R.string.detail_user_item_detail, detailUser.login)
+
+            if (detailUser.bio == null) tvBio.text = getString(R.string.null_text) else tvBio.text = detailUser.bio
+            if (detailUser.blog == null) tvBlog.text = getString(R.string.null_text) else tvBlog.text = detailUser.blog
+            if (detailUser.company == null) tvCompany.text = getString(R.string.null_text) else tvCompany.text = detailUser.company
+            if (detailUser.location == null) tvLocation.text = getString(R.string.null_text) else tvLocation.text = detailUser.location
+
+        }
+    }
+
+    private fun setVisibilityLayout(state: Boolean) {
+        if (state) {
+            with(binding.layoutItemDetailHeader) {
+                view.visibility = View.VISIBLE
+                view2.visibility = View.VISIBLE
+                tvDetailOf.visibility = View.VISIBLE
+                tvLocation.visibility = View.VISIBLE
+                tvCompany.visibility = View.VISIBLE
+                tvBlog.visibility = View.VISIBLE
+                tvBio.visibility = View.VISIBLE
+                tvFollowing.visibility = View.VISIBLE
+                tvFollowers.visibility = View.VISIBLE
+                tvFullname.visibility = View.VISIBLE
+                tvRepository.visibility = View.VISIBLE
+                textView2.visibility = View.VISIBLE
+                textView4.visibility = View.VISIBLE
+                textView6.visibility = View.VISIBLE
+                textView8.visibility = View.VISIBLE
+            }
+            binding.progressBar.visibility = View.GONE
+        } else {
+            with(binding.layoutItemDetailHeader) {
+                view.visibility = View.GONE
+                view2.visibility = View.GONE
+                tvDetailOf.visibility = View.GONE
+                tvLocation.visibility = View.GONE
+                tvCompany.visibility = View.GONE
+                tvBlog.visibility = View.GONE
+                tvBio.visibility = View.GONE
+                tvFollowing.visibility = View.GONE
+                tvFollowers.visibility = View.GONE
+                tvFullname.visibility = View.GONE
+                tvRepository.visibility = View.GONE
+                textView2.visibility = View.GONE
+                textView4.visibility = View.GONE
+                textView6.visibility = View.GONE
+                textView8.visibility = View.GONE
+            }
+            binding.progressBar.visibility = View.VISIBLE
         }
     }
 }
